66522570fdecee7e0762144a70ed001eb33a9cdf
Sohaib Irfan <tihor.personal@gmail.com>
2022-09-04 11:18:08 +0530

    Create core.py

---
+++ core.py
@@ -0,0 +1,7 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
